bbda28fca1a81f16a8fc7c2fc624233f4620de7b
Aryan Kulkarni <kulkarniaryan2129@gmail.com>
2022-11-30 22:03:42 +0530

    EveryThing done just left with adding value in business, stock and automobile. (:

---
+++ app/src/main/java/com/example/newsapplication/AutoNews.java
@@ -0,0 +1,55 @@
+package com.example.newsapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class AutoNews extends Fragment
+{
+    View view;
+    FirebaseDatabase db;
+    DatabaseReference dbr;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view =  inflater.inflate(R.layout.fragment_auto_news, container, false);
+
+        TextView newsHeading = (TextView) view.findViewById(R.id.AutoNewsHeading);
+        TextView newsContent = (TextView) view.findViewById(R.id.AutoNewsContent);
+
+        db = FirebaseDatabase.getInstance();
+        dbr = db.getReference("HomePageNews").child("automobile");
+
+        dbr.addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot)
+            {
+                String content = snapshot.getValue(String.class);
+                newsHeading.setText(content);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error)
+            {
+
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/newsapplication/newsDisplayer.java
+++ app/src/main/java/com/example/newsapplication/newsDisplayer.java
@@ -2,6 +2,7 @@ package com.example.newsapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,22 +11,48 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.w3c.dom.Text;
 
 public class newsDisplayer extends Fragment {
     View view;
+    FirebaseDatabase homePagedb;
+    DatabaseReference homepagevalue;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_news_displayer, container, false);
 
+        homePagedb = FirebaseDatabase.getInstance("https://newsapplication-936a9-default-rtdb.firebaseio.com/");
+        homepagevalue = homePagedb.getReference("HomePageNews").child("Tech");
+
         TextView homepageTitle = (TextView) view.findViewById(R.id.homePagenewsTitle);
         ImageView homePageImage = (ImageView) view.findViewById(R.id.homePagenewsImage);
         TextView homepageContent = (TextView) view.findViewById(R.id.homePageNewsContent);
 
-        homepageTitle.setText("Hello, This is first News of this app");
-        homepageContent.setText("This app is build using java in android studio");
+        homepagevalue.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String techNews = snapshot.getValue(String.class);
+                homepageContent.setText(techNews);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        homepageTitle.setText("Use of drone to transport organs between cities deemed a success\n" +
+                "by Raquel Torres\n" +
+                "November 22, 2022\n" );
+
 
         return view;
 
